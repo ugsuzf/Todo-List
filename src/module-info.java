239439301f3bdb9409a75e0dff21019072bbd109
Furkan Kaan <53346136+padawanpickle@users.noreply.github.com>
2020-03-30 15:17:15 +0200

    Add files via upload

---
+++ src/module-info.java
@@ -0,0 +1,6 @@
+module todo {
+    requires javafx.fxml;
+    requires javafx.controls;
+    requires jlfgr;
+    opens sample;
+}
---
+++ src/sample/DialogController.java
@@ -0,0 +1,32 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import sample.datamodel.TodoData;
+import sample.datamodel.TodoItem;
+
+import java.time.LocalDate;
+
+
+public class DialogController {
+    @FXML
+    private TextField shortDescriptionField;
+
+    @FXML
+    private TextArea detailsArea;
+
+    @FXML
+    private DatePicker deadlinePicker;
+
+    public TodoItem processResults() {
+        String shortDescription = shortDescriptionField.getText().trim();
+        String details = detailsArea.getText().trim();
+        LocalDate deadlineValue = deadlinePicker.getValue();
+
+        TodoItem newItem = new TodoItem(shortDescription, details, deadlineValue);
+        TodoData.getInstance().addTodoItem(newItem);
+        return newItem;
+    }
+}
